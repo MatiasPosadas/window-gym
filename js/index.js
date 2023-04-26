8450c7b7b9584ec2c80a1ade5b9d2207bc5d1dc1
Matias Posadas <93284124+MatiasPosadas@users.noreply.github.com>
2023-04-25 21:19:18 -0300

    #modulo horarios con scroll horizontal

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
-const btnZumba = document.getElementById("btn-zumba");
-const btnBachata = document.getElementById("btn-bachata");
-const zumba = document.getElementById("zumba");
-const bachata = document.getElementById("bachata");
-const clases = document.querySelector('.horarios');
+const tabs = document.querySelectorAll('.tab-btn');
+const all_content = document.querySelectorAll('.content');
 
+tabs.forEach((tab,index)=> {
+    tab.addEventListener('click', (e) => {
+        tabs.forEach(tab=>{tab.classList.remove('active')});
+        tab.classList.add('active');
 
-btnZumba.addEventListener("click", ()=> {
-        zumba.classList.add("verHorario");
-});
+        var line = document.querySelector('.line');
+line.style.width = e.target.offsetWidth + "px";
+line.style.left = e.target.offsetLeft+ "px";
+        all_content.forEach(content=>{content.classList.remove('active')});
+        all_content[index].classList.add('active');
+    })
+})
 
-btnBachata.addEventListener("click", ()=> {
-    bachata.classList.toggle("verHorario");
-});
