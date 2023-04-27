5b20287a07554fe2c3f263899044c102f6700919
Matias Posadas <93284124+MatiasPosadas@users.noreply.github.com>
2023-04-27 12:40:22 -0300

    boton redes

--- js/index.js
+++ js/index.js
@@ -18,3 +18,27 @@ tabs.forEach((tab,index)=> {
 })
 
 
+
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.enlaces__menu');
+const barras = document.querySelectorAll('.ham span');
+const logo = document.querySelector('.logo');
+const nav = document.querySelector('.nav');
+
+
+ham.addEventListener('click', ()=> {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')});
+    logo.classList.toggle('fuera');
+});
+enlaces.addEventListener('click', ()=> {
+    enlaces.classList.toggle('desactivado');
+});
+/*Nav background scroll*/
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("nav");
+    header.classList.toggle("abajo", window.scrollY>100);
+})
+
+
