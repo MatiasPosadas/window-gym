03d018abcb5b6df1ba707bef20cd1a468238330d
Matias Posadas <93284124+MatiasPosadas@users.noreply.github.com>
2023-04-27 10:45:44 -0300

    #horarios newstyle

--- js/index.js
+++ js/index.js
@@ -9,11 +9,12 @@ tabs.forEach((tab,index)=> {
         tabs.forEach(tab=>{tab.classList.remove('active')});
         tab.classList.add('active');
 
-        var line = document.querySelector('.line');
-line.style.width = e.target.offsetWidth + "px";
-line.style.left = e.target.offsetLeft+ "px";
+        //var line = document.querySelector('.line');
+//line.style.width = e.target.offsetWidth + "px";
+//line.style.left = e.target.offsetLeft+ "px";
         all_content.forEach(content=>{content.classList.remove('active')});
         all_content[index].classList.add('active');
     })
 })
+
 
